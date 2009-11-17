080b50db6ebb70559befb4450d098672b95ce2f0
Earth Quake <joco@whi.hu>
2009-11-17 19:19:57 +0100

    Added Sunken Temple is project 0% i now working

---
+++ scripts/eastern_kingdoms/sunken_temple/instance_sunken_temple.cpp
@@ -0,0 +1,27 @@
+/* Special Thanks to the k_bz Mangos-Zero, and Scriptdev2, and ScriptDev-Zero team!
+ * Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+
+/* ScriptData
+SDName: Instance_Sunken_Temple
+SD%Complete: 0
+SDComment:Place Holder
+SDCategory: Sunken Temple
+EndScriptData */
+
+#include "precompiled.h"
+#include "sunken_temple.h"
+
