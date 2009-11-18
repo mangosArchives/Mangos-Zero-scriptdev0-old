d674e2c69d2b9a137d1fc938717f008283bdf5ba
onrevax <onrevax@gmail.com>
2009-11-18 14:50:56 +0100

    Fix sql error

--- sql/scriptdev0_script_full.sql
+++ sql/scriptdev0_script_full.sql
@@ -1,10 +1,6 @@
 --
 -- Version data (part used in command .server info )
 --
-
-DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOS 8759+) ');
-
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
 -- valid entries for table are between -1000000 and -1999999
@@ -1012,21 +1008,6 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 (-1533127,'You disappoint me, students!',8858,1,0,0,'razuvious SAY_COMMAND3'),
 (-1533128,'The time for practice is over! Show me what you\'ve learned!',8859,1,0,0,'razuvious SAY_COMMAND4'),
 (-1533129,'An honorable... death...',8860,1,0,0,'razuvious SAY_DEATH');
-
-
-(-1000147,'I remember well the sting of defeat at the conclusion of the Third War. I have waited far too long for my revenge. Now the shadow of the Legion falls over this world. It is only a matter of time until all of your failed creation... is undone.',11332,1,0,0,'kazzak SAY_INTRO'),
-(-1000148,'The Legion will conquer all!',11333,1,0,0,'kazzak SAY_AGGRO1'),
-(-1000149,'All mortals will perish!',11334,1,0,0,'kazzak SAY_AGGRO2'),
-(-1000150,'All life must be eradicated!',11335,1,0,0,'kazzak SAY_SURPREME1'),
-(-1000151,'I\'ll rip the flesh from your bones!',11336,1,0,0,'kazzak SAY_SURPREME2'),
-(-1000152,'Kirel Narak!',11337,1,0,0,'kazzak SAY_KILL1'),
-(-1000153,'Contemptible wretch!',11338,1,0,0,'kazzak SAY_KILL2'),
-(-1000154,'The universe will be remade.',11339,1,0,0,'kazzak SAY_KILL3'),
-(-1000155,'The Legion... will never... fall.',11340,1,0,0,'kazzak SAY_DEATH'),
-(-1000156,'REUSE ME',0,0,0,0,'REUSE ME'),
-(-1000157,'Invaders, you dangle upon the precipice of oblivion! The Burning Legion comes and with it comes your end.',0,1,0,0,'kazzak SAY_RAND1'),
-(-1000158,'Impudent whelps, you only delay the inevitable. Where one has fallen, ten shall rise. Such is the will of Kazzak...',0,1,0,0,'kazzak SAY_RAND2');
-
 
 
 
