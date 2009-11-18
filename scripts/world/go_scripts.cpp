28aefa7868d6ca1f806673c7dd8b0d7a9f72571e
onrevax <onrevax@gmail.com>
2009-11-18 15:13:05 +0100

    Fix console error (delete code tbc wotlk)

--- scripts/world/go_scripts.cpp
+++ scripts/world/go_scripts.cpp
@@ -23,21 +23,15 @@ EndScriptData */
 
 /* ContentData
 go_cat_figurine (the "trap" version of GO, two different exist)
-go_northern_crystal_pylon
 go_eastern_crystal_pylon
 go_western_crystal_pylon
 go_barov_journal
-go_ethereum_prison
-go_ethereum_stasis
 go_field_repair_bot_74A
 go_orb_of_command
 go_resonite_cask
 go_sacred_fire_of_life
-go_shrine_of_the_birds
 go_tablet_of_madness
 go_tablet_of_the_seven
-go_tele_to_dalaran_crystal
-go_tele_to_violet_stand
 EndContentData */
 
 #include "precompiled.h"
@@ -61,19 +55,6 @@ bool GOHello_go_cat_figurine(Player* pPlayer, GameObject* pGo)
 ## go_crystal_pylons (3x)
 ######*/
 
-bool GOHello_go_northern_crystal_pylon(Player* pPlayer, GameObject* pGo)
-{
-    if (pGo->GetGoType() == GAMEOBJECT_TYPE_QUESTGIVER)
-    {
-        pPlayer->PrepareQuestMenu(pGo->GetGUID());
-        pPlayer->SendPreparedQuest(pGo->GetGUID());
-    }
-
-    if (pPlayer->GetQuestStatus(4285) == QUEST_STATUS_INCOMPLETE)
-        pPlayer->AreaExploredOrEventHappens(4285);
-
-    return true;
-}
 
 bool GOHello_go_eastern_crystal_pylon(Player* pPlayer, GameObject* pGo)
 {
@@ -116,80 +97,6 @@ bool GOHello_go_barov_journal(Player* pPlayer, GameObject* pGo)
     return true;
 }
 
-/*######
-## go_ethereum_prison
-######*/
-
-enum
-{
-    SPELL_REP_LC        = 39456,
-    SPELL_REP_SHAT      = 39457,
-    SPELL_REP_CE        = 39460,
-    SPELL_REP_CON       = 39474,
-    SPELL_REP_KT        = 39475,
-    SPELL_REP_SPOR      = 39476
-};
-
-const uint32 uiNpcPrisonEntry[] =
-{
-    22810, 22811, 22812, 22813, 22814, 22815,               //good guys
-    20783, 20784, 20785, 20786, 20788, 20789, 20790         //bad guys
-};
-
-bool GOHello_go_ethereum_prison(Player* pPlayer, GameObject* pGo)
-{
-    int iRandom = rand() % (sizeof(uiNpcPrisonEntry) / sizeof(uint32));
-
-    if (Creature* pCreature = pPlayer->SummonCreature(uiNpcPrisonEntry[iRandom],
-        pGo->GetPositionX(), pGo->GetPositionY(), pGo->GetPositionZ(), pGo->GetAngle(pPlayer),
-        TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000))
-    {
-        if (!pCreature->IsHostileTo(pPlayer))
-        {
-            uint32 uiSpell = 0;
-
-            if (FactionTemplateEntry const* pFaction = pCreature->getFactionTemplateEntry())
-            {
-                switch(pFaction->faction)
-                {
-                    case 1011: uiSpell = SPELL_REP_LC; break;
-                    case 935: uiSpell = SPELL_REP_SHAT; break;
-                    case 942: uiSpell = SPELL_REP_CE; break;
-                    case 933: uiSpell = SPELL_REP_CON; break;
-                    case 989: uiSpell = SPELL_REP_KT; break;
-                    case 970: uiSpell = SPELL_REP_SPOR; break;
-                }
-
-                if (uiSpell)
-                    pCreature->CastSpell(pPlayer,uiSpell,false);
-                else
-                    error_log("SD2: go_ethereum_prison summoned creature (entry %u) but faction (%u) are not expected by script.",pCreature->GetEntry(),pCreature->getFaction());
-            }
-        }
-    }
-
-    return false;
-}
-
-/*######
-## go_ethereum_stasis
-######*/
-
-const uint32 uiNpcStasisEntry[] =
-{
-    22825, 20888, 22827, 22826, 22828
-};
-
-bool GOHello_go_ethereum_stasis(Player* pPlayer, GameObject* pGo)
-{
-    int iRandom = rand() % (sizeof(uiNpcStasisEntry) / sizeof(uint32));
-
-    pPlayer->SummonCreature(uiNpcStasisEntry[iRandom],
-        pGo->GetPositionX(), pGo->GetPositionY(), pGo->GetPositionZ(), pGo->GetAngle(pPlayer),
-        TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
-
-    return false;
-}
 
 /*######
 ## go_field_repair_bot_74A
@@ -224,23 +131,6 @@ bool GOHello_go_gilded_brazier(Player* pPlayer, GameObject* pGO)
     return true;
 }
 
-/*######
-## go_jump_a_tron
-######*/
-
-enum
-{
-    SPELL_JUMP_A_TRON   = 33382,
-    NPC_JUMP_A_TRON     = 19041
-};
-
-bool GOHello_go_jump_a_tron(Player* pPlayer, GameObject* pGo)
-{
-    if (Creature* pCreature = GetClosestCreatureWithEntry(pGo, NPC_JUMP_A_TRON, INTERACTION_DISTANCE))
-        pCreature->CastSpell(pPlayer, SPELL_JUMP_A_TRON, false);
-
-    return false;
-}
 
 /*######
 ## go_orb_of_command
@@ -289,46 +179,6 @@ bool GOHello_go_sacred_fire_of_life(Player* pPlayer, GameObject* pGO)
 }
 
 /*######
-## go_shrine_of_the_birds
-######*/
-
-enum
-{
-    NPC_HAWK_GUARD      = 22992,
-    NPC_EAGLE_GUARD     = 22993,
-    NPC_FALCON_GUARD    = 22994,
-    GO_SHRINE_HAWK      = 185551,
-    GO_SHRINE_EAGLE     = 185547,
-    GO_SHRINE_FALCON    = 185553
-};
-
-bool GOHello_go_shrine_of_the_birds(Player* pPlayer, GameObject* pGo)
-{
-    uint32 uiBirdEntry = 0;
-
-    float fX,fY,fZ;
-    pGo->GetClosePoint(fX,fY,fZ,pGo->GetObjectSize(),INTERACTION_DISTANCE);
-
-    switch(pGo->GetEntry())
-    {
-        case GO_SHRINE_HAWK:
-            uiBirdEntry = NPC_HAWK_GUARD;
-            break;
-        case GO_SHRINE_EAGLE:
-            uiBirdEntry = NPC_EAGLE_GUARD;
-            break;
-        case GO_SHRINE_FALCON:
-            uiBirdEntry = NPC_FALCON_GUARD;
-            break;
-    }
-
-    if (uiBirdEntry)
-        pPlayer->SummonCreature(uiBirdEntry, fX, fY, fZ, pGo->GetOrientation(), TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 60000);
-
-    return false;
-}
-
-/*######
 ## go_tablet_of_madness
 ######*/
 
@@ -357,36 +207,6 @@ bool GOHello_go_tablet_of_the_seven(Player* pPlayer, GameObject* pGo)
     return true;
 }
 
-/*######
-## go_tele_to_dalaran_crystal
-######*/
-
-enum
-{
-    QUEST_LEARN_LEAVE_RETURN    = 12790,
-    QUEST_TELE_CRYSTAL_FLAG     = 12845
-};
-
-bool GOHello_go_tele_to_dalaran_crystal(Player* pPlayer, GameObject* pGo)
-{
-    if (pPlayer->GetQuestRewardStatus(QUEST_TELE_CRYSTAL_FLAG))
-        return false;
-
-    //TODO: must send error message (what kind of message? On-screen?)
-    return true;
-}
-
-/*######
-## go_tele_to_violet_stand
-######*/
-
-bool GOHello_go_tele_to_violet_stand(Player* pPlayer, GameObject* pGo)
-{
-    if (pPlayer->GetQuestRewardStatus(QUEST_LEARN_LEAVE_RETURN) || pPlayer->GetQuestStatus(QUEST_LEARN_LEAVE_RETURN) == QUEST_STATUS_INCOMPLETE)
-        return false;
-
-    return true;
-}
 
 void AddSC_go_scripts()
 {
@@ -395,11 +215,6 @@ void AddSC_go_scripts()
     newscript = new Script;
     newscript->Name = "go_cat_figurine";
     newscript->pGOHello =           &GOHello_go_cat_figurine;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "go_northern_crystal_pylon";
-    newscript->pGOHello =           &GOHello_go_northern_crystal_pylon;
     newscript->RegisterSelf();
 
     newscript = new Script;
@@ -418,16 +233,6 @@ void AddSC_go_scripts()
     newscript->RegisterSelf();
 
     newscript = new Script;
-    newscript->Name = "go_ethereum_prison";
-    newscript->pGOHello =           &GOHello_go_ethereum_prison;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "go_ethereum_stasis";
-    newscript->pGOHello =           &GOHello_go_ethereum_stasis;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
     newscript->Name = "go_field_repair_bot_74A";
     newscript->pGOHello =           &GOHello_go_field_repair_bot_74A;
     newscript->RegisterSelf();
@@ -435,11 +240,6 @@ void AddSC_go_scripts()
     newscript = new Script;
     newscript->Name = "go_gilded_brazier";
     newscript->pGOHello =           &GOHello_go_gilded_brazier;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "go_jump_a_tron";
-    newscript->pGOHello =           &GOHello_go_jump_a_tron;
     newscript->RegisterSelf();
 
     newscript = new Script;
@@ -458,11 +258,6 @@ void AddSC_go_scripts()
     newscript->RegisterSelf();
 
     newscript = new Script;
-    newscript->Name = "go_shrine_of_the_birds";
-    newscript->pGOHello =           &GOHello_go_shrine_of_the_birds;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
     newscript->Name = "go_tablet_of_madness";
     newscript->pGOHello =           &GOHello_go_tablet_of_madness;
     newscript->RegisterSelf();
@@ -472,13 +267,4 @@ void AddSC_go_scripts()
     newscript->pGOHello =           &GOHello_go_tablet_of_the_seven;
     newscript->RegisterSelf();
 
-    newscript = new Script;
-    newscript->Name = "go_tele_to_dalaran_crystal";
-    newscript->pGOHello =           &GOHello_go_tele_to_dalaran_crystal;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "go_tele_to_violet_stand";
-    newscript->pGOHello =           &GOHello_go_tele_to_violet_stand;
-    newscript->RegisterSelf();
 }
--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -9,11 +9,6 @@ extern void AddSC_battleground();
 
 //custom
 
-//examples
-extern void AddSC_example_creature();
-extern void AddSC_example_escort();
-extern void AddSC_example_gossip_codebox();
-extern void AddSC_example_misc();
 
 //world
 extern void AddSC_areatrigger_scripts();
