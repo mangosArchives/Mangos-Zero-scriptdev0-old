496edcd5fd1b8707a37e441c12d9d403bf4ced5e
nofantasy <nofantasy@5f9c896b-1e26-0410-94da-f77f675e2462>
2010-03-06 17:15:44 +0000

    [z0072] Add more generic (and accurate) function to know if unit is in left or right side in gothik combat area. Original code by Schmoozerd Some minor additions to instance definitions.
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1635 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/eastern_kingdoms/naxxramas/boss_gothik.cpp
+++ scripts/eastern_kingdoms/naxxramas/boss_gothik.cpp
@@ -76,6 +76,7 @@ struct MANGOS_DLL_DECL boss_gothikAI : public ScriptedAI
     {
         m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
         m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
+        //SetCombatMovement(false);                         // uncomment when more parts implemented.
         Reset();
     }
 
@@ -118,11 +119,11 @@ struct MANGOS_DLL_DECL boss_gothikAI : public ScriptedAI
         {
             if (Creature* pTrigger = m_pInstance->instance->GetCreature(*itr))
             {
-                // not valid
+                // not valid for summon point
                 if (pTrigger->GetPositionZ() >= (m_creature->GetPositionZ() - 5.0f))
                     continue;
 
-                if (m_creature->GetAngle(pTrigger) >= M_PI_F)
+                if (m_pInstance->IsInRightSideGothArea(pTrigger))
                     m_lRightSideTrigger.push_back(*itr);
                 else
                     m_lLeftSideTrigger.push_back(*itr);
