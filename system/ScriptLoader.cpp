Nanac/Scriptdev2

Add struct and needed definitions for boss_gothik

system/ScriptLoader.cpp
@@ -270,7 +270,7 @@ extern void AddSC_boss_anubrekhan();                        //naxxramas
 extern void AddSC_boss_four_horsemen();
 extern void AddSC_boss_faerlina();
 extern void AddSC_boss_gluth();
-extern void AddSC_howling_fjord();
+extern void AddSC_boss_gothik();
 extern void AddSC_boss_kelthuzad();
 extern void AddSC_boss_loatheb();
 extern void AddSC_boss_maexxna();
@@ -313,6 +313,7 @@ extern void AddSC_borean_tundra();
 extern void AddSC_dalaran();
 extern void AddSC_dragonblight();
 extern void AddSC_grizzly_hills();
+extern void AddSC_howling_fjord();
 extern void AddSC_icecrown();
 extern void AddSC_sholazar_basin();
 extern void AddSC_storm_peaks();
@@ -664,7 +665,7 @@ void AddScripts()
     AddSC_boss_four_horsemen();
     AddSC_boss_faerlina();
     AddSC_boss_gluth();
-    AddSC_howling_fjord();
+    AddSC_boss_gothik();
     AddSC_boss_kelthuzad();
     AddSC_boss_loatheb();
     AddSC_boss_maexxna();
@@ -707,6 +708,7 @@ void AddScripts()
     AddSC_dalaran();
     AddSC_dragonblight();
     AddSC_grizzly_hills();
+    AddSC_howling_fjord();
     AddSC_icecrown();
     AddSC_sholazar_basin();
     AddSC_storm_peaks();

mangosArchives/Mangos-Zero-scriptdev0-old

git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1629 5f9c896b-1e26-0410-94da-f77f675e2462

system/ScriptLoader.cpp
@@ -69,17 +69,19 @@ extern void AddSC_boss_majordomo();
 extern void AddSC_boss_ragnaros();
 extern void AddSC_instance_molten_core();
 extern void AddSC_molten_core();
-extern void AddSC_boss_anubrekhan(); //Naxxramas
+extern void AddSC_boss_anubrekhan();                        //naxxramas
+extern void AddSC_boss_four_horsemen();
+extern void AddSC_boss_faerlina();
+extern void AddSC_boss_gluth();
+extern void AddSC_boss_gothik();
+extern void AddSC_boss_kelthuzad();
+extern void AddSC_boss_loatheb();
 extern void AddSC_boss_maexxna();
+extern void AddSC_boss_noth();
 extern void AddSC_boss_patchwerk();
 extern void AddSC_boss_razuvious();
-extern void AddSC_boss_kelthuzad();
-extern void AddSC_boss_faerlina();
-extern void AddSC_boss_loatheb();
-extern void AddSC_boss_noth();
-extern void AddSC_boss_gluth();
 extern void AddSC_boss_sapphiron();
-extern void AddSC_boss_four_horsemen();
+extern void AddSC_instance_naxxramas();
 extern void AddSC_boss_arcanist_doan();                     //scarlet_monastery
 extern void AddSC_boss_azshir_the_sleepless();
 extern void AddSC_boss_bloodmage_thalnos();
@@ -204,7 +206,6 @@ extern void AddSC_thunder_bluff();
 extern void AddSC_ungoro_crater();
 extern void AddSC_winterspring();
 
-
 void AddScripts()
 {
     //battlegrounds
@@ -271,17 +272,19 @@ void AddScripts()
     AddSC_boss_ragnaros();
     AddSC_instance_molten_core();
     AddSC_molten_core();
-    AddSC_boss_anubrekhan();   //Naxxramas
+    AddSC_boss_anubrekhan();                                //naxxramas
+    AddSC_boss_four_horsemen();
+    AddSC_boss_faerlina();
+    AddSC_boss_gluth();
+    AddSC_boss_gothik();
+    AddSC_boss_kelthuzad();
+    AddSC_boss_loatheb();
     AddSC_boss_maexxna();
+    AddSC_boss_noth();
     AddSC_boss_patchwerk();
     AddSC_boss_razuvious();
-    AddSC_boss_kelthuzad();
-    AddSC_boss_faerlina();
-    AddSC_boss_loatheb();
-    AddSC_boss_noth();
-    AddSC_boss_gluth();
     AddSC_boss_sapphiron();
-    AddSC_boss_four_horsemen();
+    AddSC_instance_naxxramas();
     AddSC_boss_arcanist_doan();                             //scarlet_monastery
     AddSC_boss_azshir_the_sleepless();
     AddSC_boss_bloodmage_thalnos();
@@ -404,6 +407,5 @@ void AddScripts()
     AddSC_thunder_bluff();
     AddSC_ungoro_crater();
     AddSC_winterspring();
-
 
 }