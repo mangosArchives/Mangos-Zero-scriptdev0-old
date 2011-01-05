73c41caf765aa2514aa01f1ea9d20ccf97535c85
Yehonal <hw.2@hotmail.it>
2011-01-05 15:14:59 +0100

    [z0131] cleaned revision_sql.h

--- system/revision_sql.h
+++ system/revision_sql.h
@@ -1,5 +1,4 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_SCRIPTDEVZERO "required_z0116_r1785_scriptdevzero"
  #define  "required_z0129_r1704_mangoszero"
 #endif // __REVISION_SQL_H__
