c704f6e9af274419dd2895d38e9afd12446f3906
Yehonal <hw.2@hotmail.it>
2011-01-06 00:02:00 +0100

    [z0147] Add scriptname to boss_maexxena web wrap with some minor adjustments
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1726 5f9c896b-1e26-0410-94da-f77f675e2462
    
    Conflicts:
    
    	scripts/eastern_kingdoms/naxxramas/boss_maexxna.cpp
    	sql/Updates/Makefile.am

--- system/revision_sql.h
+++ system/revision_sql.h
@@ -1,5 +1,5 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_SCRIPTDEVZERO "required_z0135_r1715_scriptdevzero"
- #define  "required_z0135_r1715_mangoszero"
+ #define  "required_z0147_r1726_mangoszero"
 #endif // __REVISION_SQL_H__
