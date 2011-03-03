3359be636bbdeab7dbdfd2468b98d5ae5f34eb10
Salja <beleko2009@web.de>
2011-03-03 23:27:01 +0100

    [z0188] Improve the security for the MySQL DB. Patch by DasBlub
    Signed-off-by: Salja <beleko2009@web.de>

--- sql/scriptdevzero_create_database.sql
+++ sql/scriptdevzero_create_database.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE `scriptdevzero` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON `scriptdevzero` . * TO 'mangos'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES ON `scriptdevzero`.* TO 'mangos'@'localhost';
 
