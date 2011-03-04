ce1a356405b9e08b9842772164641dc67f1d8606
Salja <beleko2009@web.de>
2011-03-04 15:09:42 +0100

    [z0196] Typo from last Commit.
    Signed-off-by: Salja <beleko2009@web.de>

--- sql/mangos_scriptname_full.sql
+++ sql/mangos_scriptname_full.sql
@@ -151,7 +151,7 @@ UPDATE creature_template SET ScriptName ='npc_dughal_stormwing' WHERE entry=9022
 UPDATE creature_template SET ScriptName ='npc_marshal_windsor' WHERE entry=9023;
 UPDATE creature_template SET ScriptName ='npc_marshal_reginald_windsor' WHERE entry=9682;
 UPDATE creature_template SET ScriptName ='npc_tobias_seecher' WHERE entry=9679;
-UPDATE gameobject_template SET ScriptName ='go_cell_door' WHERE entry IN (170561, 170562, 170566, 170567, 170568, 170569)
+UPDATE gameobject_template SET ScriptName ='go_cell_door' WHERE entry IN (170561, 170562, 170566, 170567, 170568, 170569);
 
 /* BLACKROCK SPIRE */
 UPDATE instance_template SET ScriptName='instance_blackrock_spire' WHERE map=229;
