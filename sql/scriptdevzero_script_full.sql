cb1221cedebd3235268c245ed326ef427691d9e3
Salja <beleko2009@web.de>
2011-03-04 23:17:54 +0100

    [z0198] Backport: Scholomance. Thanks to all Authors and Chero for Backporting.
    Signed-off-by: Salja <beleko2009@web.de>

--- sql/scriptdevzero_script_full.sql
+++ sql/scriptdevzero_script_full.sql
@@ -817,6 +817,8 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 
 
 -- -1 289 000 SCHOLOMANCE
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1289000,'School is in session!',0,1,0,0,'gandling SAY_GANDLING_SPAWN');
 
 -- -1 309 000 ZUL'GURUB
 INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
