ea729cf94936c68b9b35a4505415c972d243c552
Salja <beleko2009@web.de>
2011-03-22 12:47:32 +0100

    [z0217] Update ScriptDev2 Copyright notice.
    Signed-off-by: Salja <beleko2009@web.de>

--- scripts/world/boss_lord_kazzak.cpp
+++ scripts/world/boss_lord_kazzak.cpp
@@ -1,5 +1,5 @@
 /* Special Thanks the Mangos-Zero, and Scriptdev2, and ScriptDev-Zero team!
- * Copyright (C) 2006 - 2011 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2006 - 2011 ScriptDev2 <http://www.scriptdev2.com/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
