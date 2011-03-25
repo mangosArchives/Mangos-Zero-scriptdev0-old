5eb5acd5b5ecbb232b842badeddb153ca14159ed
Salja <beleko2009@web.de>
2011-03-25 15:45:12 +0100

    [z0223] Backport: Cleanup instance script for Zul'Gurub. Thanks Xfurry
    Also implement two zone wide yells
    
    Signed-off-by: Salja <beleko2009@web.de>

--- sql/mangos_scriptname_full.sql
+++ sql/mangos_scriptname_full.sql
@@ -17,6 +17,10 @@ DELETE FROM scripted_areatrigger WHERE entry IN (2026, 2046);
 INSERT INTO scripted_areatrigger VALUES
 (2026, 'at_blackrock_spire'),
 (2046, 'at_blackrock_spire');
+DELETE FROM scripted_areatrigger WHERE entry IN (3958, 3960);
+INSERT INTO scripted_areatrigger VALUES
+(3958, 'at_zulgurub'),
+(3960, 'at_zulgurub');
 
 /* BATTLEGROUNDS */
 UPDATE creature_template SET ScriptName='npc_spirit_guide' WHERE entry IN (13116, 13117);
