09ed33b3b461c31db9ef51351d7f92d98a3fd75e
Salja <beleko2009@web.de>
2011-04-19 00:02:56 +0200

    [z0272] Use SpellAuraHolder for "boss_pyroguard_emberseer.cpp".
      *MaNGOS have yet SpellAuraHolder sice [z1478].
    
    Signed-off-by: Salja <beleko2009@web.de>

--- scripts/eastern_kingdoms/blackrock_spire/boss_pyroguard_emberseer.cpp
+++ scripts/eastern_kingdoms/blackrock_spire/boss_pyroguard_emberseer.cpp
@@ -152,9 +152,9 @@ bool EffectDummyCreature_pyroguard_emberseer(Unit* pCaster, uint32 uiSpellId, Sp
     }
     else if (uiSpellId == SPELL_GROWING && uiEffIndex == EFFECT_INDEX_0)
     {
-        if (pCreatureTarget->HasAura(SPELL_GROWING, EFFECT_INDEX_1))
+        if(SpellAuraHolder* pGrow = pCreatureTarget->GetSpellAuraHolder(SPELL_GROWING))
         {
-            if (pCreatureTarget->GetAura(SPELL_GROWING, EFFECT_INDEX_1)->GetStackAmount() == MAX_GROWING_STACKS)
+            if(pGrow->GetStackAmount() == MAX_GROWING_STACKS)
             {
                 pCreatureTarget->CastSpell(pCreatureTarget, SPELL_FULL_STRENGTH, true);
                 pCreatureTarget->CastSpell(pCreatureTarget, SPELL_TRANSFORM, true);
