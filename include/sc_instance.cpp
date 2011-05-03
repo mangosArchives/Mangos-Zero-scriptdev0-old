a891075204b55069f7499875b9e36346af3b9b7c
Chero <3dserver@post.sk>
2011-05-03 15:47:18 +0200

    [z0305] Add new Wrapper to Open and Close doors or Use and Reset buttons
    * These functions are not meant to replace DoUseDoorOrButton, but for cases in which the order of opening/ closing is not given
    * Original commit (192b447)
    
    Signed-off-by: Chero <3dserver@post.sk>

--- include/sc_instance.cpp
+++ include/sc_instance.cpp
@@ -35,7 +35,7 @@ void ScriptedInstance::DoUseDoorOrButton(uint64 uiGuid, uint32 uiWithRestoreTime
                 pGo->ResetDoorOrButton();
         }
         else
-            error_log("SD2: Script call DoUseDoorOrButton, but gameobject entry %u is type %u.",pGo->GetEntry(),pGo->GetGoType());
+            error_log("SD0: Script call DoUseDoorOrButton, but gameobject entry %u is type %u.",pGo->GetEntry(),pGo->GetGoType());
     }
 }
 
@@ -69,7 +69,7 @@ void ScriptedInstance::DoUpdateWorldState(uint32 uiStateId, uint32 uiStateData)
         }
     }
     else
-        debug_log("SD2: DoUpdateWorldState attempt send data but no players in map.");
+        debug_log("SD0: DoUpdateWorldState attempt send data but no players in map.");
 }
 
 Player* ScriptedInstance::GetPlayerInMap(bool bOnlyAlive /*=false*/, bool bCanBeGamemaster /*=true*/)
@@ -84,4 +84,40 @@ Player* ScriptedInstance::GetPlayerInMap(bool bOnlyAlive /*=false*/, bool bCanBe
     }
 
     return NULL;
+}
+
+// Open door or Use button - Optional uiWithRestoreTime. If not defined, autoCloseTime will be used (if not 0 by default in *_template)
+void ScriptedInstance::DoOpenDoorOrButton(uint64 uiGuid, uint32 uiWithRestoreTime, bool bUseAlternativeState)
+{
+    if (!uiGuid)
+        return;
+
+    if (GameObject* pGo = instance->GetGameObject(uiGuid))
+    {
+        if (pGo->GetGoType() == GAMEOBJECT_TYPE_DOOR || pGo->GetGoType() == GAMEOBJECT_TYPE_BUTTON)
+        {
+            if (pGo->getLootState() == GO_READY)
+                pGo->UseDoorOrButton(uiWithRestoreTime, bUseAlternativeState);
+        }
+        else
+            error_log("SD0: Script call DoOpenDoorOrButton, but gameobject entry %u is type %u.",pGo->GetEntry(),pGo->GetGoType());
+    }
+}
+
+// Close door or Reset button
+void ScriptedInstance::DoCloseDoorOrButton(uint64 uiGuid)
+{
+    if (!uiGuid)
+        return;
+
+    if (GameObject* pGo = instance->GetGameObject(uiGuid))
+    {
+        if (pGo->GetGoType() == GAMEOBJECT_TYPE_DOOR || pGo->GetGoType() == GAMEOBJECT_TYPE_BUTTON)
+        {
+            if (pGo->getLootState() == GO_ACTIVATED)
+                pGo->ResetDoorOrButton();
+        }
+        else
+            error_log("SD0: Script call DoCloseDoorOrButton, but gameobject entry %u is type %u.",pGo->GetEntry(),pGo->GetGoType());
+    }
 }
--- include/sc_instance.h
+++ include/sc_instance.h
@@ -32,11 +32,11 @@ enum EncounterState
     SPECIAL       = 4
 };
 
-#define OUT_SAVE_INST_DATA             debug_log("SD2: Saving Instance Data for Instance %s (Map %d, Instance Id %d)", instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
-#define OUT_SAVE_INST_DATA_COMPLETE    debug_log("SD2: Saving Instance Data for Instance %s (Map %d, Instance Id %d) completed.", instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
-#define OUT_LOAD_INST_DATA(a)          debug_log("SD2: Loading Instance Data for Instance %s (Map %d, Instance Id %d). Input is '%s'", instance->GetMapName(), instance->GetId(), instance->GetInstanceId(), a)
-#define OUT_LOAD_INST_DATA_COMPLETE    debug_log("SD2: Instance Data Load for Instance %s (Map %d, Instance Id: %d) is complete.",instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
-#define OUT_LOAD_INST_DATA_FAIL        error_log("SD2: Unable to load Instance Data for Instance %s (Map %d, Instance Id: %d).",instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
+#define OUT_SAVE_INST_DATA             debug_log("SD0: Saving Instance Data for Instance %s (Map %d, Instance Id %d)", instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
+#define OUT_SAVE_INST_DATA_COMPLETE    debug_log("SD0: Saving Instance Data for Instance %s (Map %d, Instance Id %d) completed.", instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
+#define OUT_LOAD_INST_DATA(a)          debug_log("SD0: Loading Instance Data for Instance %s (Map %d, Instance Id %d). Input is '%s'", instance->GetMapName(), instance->GetId(), instance->GetInstanceId(), a)
+#define OUT_LOAD_INST_DATA_COMPLETE    debug_log("SD0: Instance Data Load for Instance %s (Map %d, Instance Id: %d) is complete.",instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
+#define OUT_LOAD_INST_DATA_FAIL        error_log("SD0: Unable to load Instance Data for Instance %s (Map %d, Instance Id: %d).",instance->GetMapName(), instance->GetId(), instance->GetInstanceId())
 
 class MANGOS_DLL_DECL ScriptedInstance : public InstanceData
 {
@@ -47,6 +47,12 @@ class MANGOS_DLL_DECL ScriptedInstance : public InstanceData
 
         // Change active state of doors or buttons
         void DoUseDoorOrButton(uint64 uiGuid, uint32 uiWithRestoreTime = 0, bool bUseAlternativeState = false);
+
+        // Open door or Use button
+        void DoOpenDoorOrButton(uint64 uiGuid, uint32 uiWithRestoreTime = 0, bool bUseAlternativeState = false);
+
+        // Close door or Reset button
+        void DoCloseDoorOrButton(uint64 uiGuid);
 
         // Respawns a GO having negative spawntimesecs in gameobject-table
         void DoRespawnGameObject(uint64 uiGuid, uint32 uiTimeToDespawn = MINUTE);
