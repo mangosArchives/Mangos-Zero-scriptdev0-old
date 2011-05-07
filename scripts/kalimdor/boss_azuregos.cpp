ca8b6d7b12b41e6bae43218358ceae01cd746a55
Salja <beleko2009@web.de>
2011-05-07 18:20:15 +0200

    [z0320] Azuregos - cleanup & improve. Patch by Xfurry
    Summary:
    * basic cleanup
    * remove arcane vacuum hack -> will be done in core
    * add support for Mark of Frost -> will be done in core
    
    Signed-off-by: Salja <beleko2009@web.de>

--- scripts/kalimdor/boss_azuregos.cpp
+++ scripts/kalimdor/boss_azuregos.cpp
@@ -20,7 +20,7 @@
 /* ScriptData
 SDName: Boss_Azuregos
 SD%Complete: 90
-SDComment: Mark of Frost missing core support?
+SDComment: Teleport not included, spell reflect not effecting dots (Core problem)
 SDCategory: Azshara
 EndScriptData */
 
@@ -28,114 +28,112 @@ EndScriptData */
 
 enum
 {
-    SAY_TELEPORT         = -1000100,
+    SAY_TELEPORT                = -1000100,
 
-    //SPELL_MARKOFFROST    = 23182,
-    SPELL_MANASTORM      = 21097,
-    SPELL_CHILL          = 21098,
-    SPELL_FROSTBREATH    = 21099,
-    SPELL_REFLECTION     = 22067,
-    SPELL_CLEAVE         = 19983,
-    SPELL_ENRAGE         = 23537,
-    //SPELL_ARCANE_VACUUM  = 21147
+    SPELL_ARCANE_VACUUM         = 21147,
+    SPELL_MARK_OF_FROST_PLAYER  = 23182,
+    SPELL_MARK_OF_FROST         = 23184,        // triggers 23186 on players that have 23182; unfortunatelly 23183 is missing from dbc
+    SPELL_MANA_STORM            = 21097,
+    SPELL_CHILL                 = 21098,
+    SPELL_FROST_BREATH          = 21099,
+    SPELL_REFLECT               = 22067,
+    SPELL_CLEAVE                = 19983,        // old id = 8255; this one is from wowhead and seems to be the correct one
+    SPELL_ENRAGE                = 23537,
 };
 
 struct MANGOS_DLL_DECL boss_azuregosAI : public ScriptedAI
 {
     boss_azuregosAI(Creature* pCreature) : ScriptedAI(pCreature) {Reset();}
 
-    uint32 m_uiMarkOfFrostTimer;
     uint32 m_uiManaStormTimer;
     uint32 m_uiChillTimer;
     uint32 m_uiBreathTimer;
     uint32 m_uiTeleportTimer;
     uint32 m_uiReflectTimer;
     uint32 m_uiCleaveTimer;
-    uint32 m_uiEnrageTimer;
     bool m_bEnraged;
 
     void Reset()
     {
-        m_uiMarkOfFrostTimer = 35000;
-        m_uiManaStormTimer = urand(5000, 17000);
-        m_uiChillTimer = urand(10000, 30000);
-        m_uiBreathTimer = urand(2000, 8000);
-        m_uiTeleportTimer = 30000;
-        m_uiReflectTimer = urand(15000, 30000);
-        m_uiCleaveTimer = 7000;
-        m_uiEnrageTimer = 0;
-        m_bEnraged = false;
+        m_uiManaStormTimer  = urand(5000, 17000);
+        m_uiChillTimer      = urand(10000, 30000);
+        m_uiBreathTimer     = urand(2000, 8000);
+        m_uiTeleportTimer   = 30000;
+        m_uiReflectTimer    = urand(15000, 30000);
+        m_uiCleaveTimer     = 7000;
+        m_bEnraged          = false;
+    }
+
+    void KilledUnit(Unit* pVictim)
+    {
+        // this debuff is set on player to make it vulnerable to mark of frost;
+        // if the player resurrects during the fight he'll get stunned
+        pVictim->CastSpell(pVictim, SPELL_MARK_OF_FROST_PLAYER, true);
+    }
+
+    void Aggro(Unit* pWho)
+    {
+        // boss aura which triggers the stun effect on dead players who resurrect
+        DoCastSpellIfCan(m_creature, SPELL_MARK_OF_FROST);
     }
 
     void UpdateAI(const uint32 uiDiff)
     {
-        //Return since we have no target
+        // Return since we have no target
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
         if (m_uiTeleportTimer < uiDiff)
         {
-            DoScriptText(SAY_TELEPORT, m_creature);
-
-            ThreatList const& m_lThreatList = m_creature->getThreatManager().getThreatList();
-            for (ThreatList::const_iterator itr = m_lThreatList.begin();itr != m_lThreatList.end(); ++itr)
+            if (DoCastSpellIfCan(m_creature, SPELL_ARCANE_VACUUM) == CAST_OK)
             {
-                Unit* pUnit = m_creature->GetMap()->GetUnit((*itr)->getUnitGuid());
-
-                if (pUnit && (pUnit->GetTypeId() == TYPEID_PLAYER && m_creature->IsWithinDistInMap(pUnit, 30.0f)))
-                {
-                    DoTeleportPlayer(pUnit, m_creature->GetPositionX(), m_creature->GetPositionY(), m_creature->GetPositionZ()+3, pUnit->GetOrientation());
-                    (*itr)->removeReference(); // Reset threat for the current player?
-                }
+                DoScriptText(SAY_TELEPORT, m_creature);
+                m_uiTeleportTimer = 30000;
             }
-
-            m_uiTeleportTimer = 30000;
         }
         else
             m_uiTeleportTimer -= uiDiff;
 
-        //        //MarkOfFrostTimer
-        //        if (MarkOfFrostTimer < uiDiff)
-        //        {
-        //            DoCastSpellIfCan(m_creature->getVictim(),SPELL_MARKOFFROST);
-        //            MarkOfFrostTimer = 25000;
-        //        }else MarkOfFrostTimer -= uiDiff;
-
+        // Chill_Timer
         if (m_uiChillTimer < uiDiff)
         {
-            if (DoCastSpellIfCan(m_creature->getVictim(),SPELL_CHILL) == CAST_OK)
+            if (DoCastSpellIfCan(m_creature, SPELL_CHILL) == CAST_OK)
                 m_uiChillTimer = urand(13000, 25000);
         }
         else
             m_uiChillTimer -= uiDiff;
 
+        // Breath_Timer
         if (m_uiBreathTimer < uiDiff)
         {
-            if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_FROSTBREATH) == CAST_OK)
+            if (DoCastSpellIfCan(m_creature, SPELL_FROST_BREATH) == CAST_OK)
                 m_uiBreathTimer = urand(10000, 15000);
         }
         else
             m_uiBreathTimer -= uiDiff;
 
+        // ManaStorm_Timer
         if (m_uiManaStormTimer < uiDiff)
         {
-            if (Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM,0))
             {
-                if (DoCastSpellIfCan(target, SPELL_MANASTORM) == CAST_OK)
+                if (DoCastSpellIfCan(pTarget, SPELL_MANA_STORM) == CAST_OK)
                     m_uiManaStormTimer = urand(7500, 12500);
             }
         }
         else
             m_uiManaStormTimer -= uiDiff;
 
+        // Reflect_Timer
         if (m_uiReflectTimer < uiDiff)
         {
-            if (DoCastSpellIfCan(m_creature, SPELL_REFLECTION) == CAST_OK)
+            if (DoCastSpellIfCan(m_creature, SPELL_REFLECT) == CAST_OK)
                 m_uiReflectTimer = urand(20000, 35000);
         }
         else
             m_uiReflectTimer -= uiDiff;
 
+        // Cleave_Timer
         if (m_uiCleaveTimer < uiDiff)
         {
             if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_CLEAVE) == CAST_OK)
@@ -144,6 +142,7 @@ struct MANGOS_DLL_DECL boss_azuregosAI : public ScriptedAI
         else
             m_uiCleaveTimer -= uiDiff;
 
+        // Enrage_Timer
         if (m_creature->GetHealthPercent() < 26.0f && !m_bEnraged)
         {
             if (DoCastSpellIfCan(m_creature, SPELL_ENRAGE) == CAST_OK)
@@ -153,6 +152,7 @@ struct MANGOS_DLL_DECL boss_azuregosAI : public ScriptedAI
         DoMeleeAttackIfReady();
     }
 };
+
 CreatureAI* GetAI_boss_azuregos(Creature* pCreature)
 {
     return new boss_azuregosAI(pCreature);
