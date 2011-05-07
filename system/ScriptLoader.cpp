10c153b4111fb281b9724f7d96b22b2760477336
Salja <beleko2009@web.de>
2011-05-07 09:17:05 +0200

    [z0319] Backport: Razorfen Kraul - Implement Escort quest 1144. Patch by Chero
    * Add script npc_willix_the_importer
    * Waypoints based on trinity by guenex
    * Thanks to Xfurry for providing full patch
    * "Aggro" texts are not entirely understood and might not be accurate :(
    
    Signed-off-by: Salja <beleko2009@web.de>

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -185,6 +185,7 @@ extern void AddSC_boss_onyxia();                            //onyxias_lair
 extern void AddSC_boss_amnennar_the_coldbringer();          //razorfen_downs
 extern void AddSC_razorfen_downs();
 extern void AddSC_instance_razorfen_kraul();                //razorfen_kraul
+extern void AddSC_razorfen_kraul();
 extern void AddSC_boss_ayamiss();                           //ruins_of_ahnqiraj
 extern void AddSC_boss_buru();
 extern void AddSC_boss_kurinnaxx();
@@ -397,6 +398,7 @@ void AddScripts()
     AddSC_boss_amnennar_the_coldbringer();                  //razorfen_downs
     AddSC_razorfen_downs();
     AddSC_instance_razorfen_kraul();                        //razorfen_kraul
+    AddSC_razorfen_kraul();
     AddSC_boss_ayamiss();                                   //ruins_of_ahnqiraj
     AddSC_boss_buru();
     AddSC_boss_kurinnaxx();
