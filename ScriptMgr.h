0e44da1ff9e163d65c4c985ae45ac1e8a6116f07
Salja <belekeo2009@web.de>
2011-05-31 16:49:15 +0200

    [z0386] Implement new Guid Storage for GO and NPC ObjectGuids
    * Use a std::map to store Guid by entry for GOs and NPCs
      This is based on ideas by Darkstalker
    * Add Accessor functions to directly get a GameObject* or Creature* for stored entries
    * Add Wrapper functions for DoUseDoorOrButton, DoRespawnGameObject to use them with GO-Entry
    * Add some doxygen comments for sc_instance
    
    Signed-off-by: Salja <belekeo2009@web.de>

--- ScriptMgr.h
+++ ScriptMgr.h
@@ -36,6 +36,7 @@ class Unit;
 class WorldObject;
 class Aura;
 class Object;
+class ObjectGuid;
 
 // *********************************************************
 // ************** Some defines used globally ***************
@@ -47,6 +48,7 @@ class Object;
 typedef std::list<uint64> GUIDList;
 typedef std::set<uint64> GUIDSet;
 typedef std::vector<uint64> GUIDVector;
+typedef std::map<uint32, ObjectGuid> EntryGuidMap;
 
 /* Escort Factions
  * TODO: find better namings and definitions.
