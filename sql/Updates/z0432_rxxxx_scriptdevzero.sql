4fce230f53c290782dd0c246f9cfbdde87ca3bbd
Chero <3dserver@post.sk>
2011-06-25 11:10:04 +0200

    [z0433] Fixed two typos in SQL update.
    Signed-off-by: Chero <3dserver@post.sk>

--- sql/Updates/z0432_rxxxx_scriptdevzero.sql
+++ sql/Updates/z0432_rxxxx_scriptdevzero.sql
@@ -1,2 +1,3 @@
-DELETE FROM script_texts WHERE entry=-1409018; 
+DELETE FROM script_texts WHERE entry=-1409018;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
 (-1409018,'MY PATIENCE IS DWINDLING! COME, GNATS, TO YOUR DEATH!',8048,1,0,0,'ragnaros SAY_MAGMABURST');
