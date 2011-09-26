2a0461fac6ee676cf717157b8d02e77caec6735f
Salja <beleko2009@web.de>
2011-09-26 21:54:40 +0200

    [z0472] Backport: Improve some things in Deadmines. Thanks stfx * Mr. Smite: Fix delay handling after he "used" his chest * Cannon event: Improve timed handling, make code more default, fix say types
    Signed-off-by: Salja <beleko2009@web.de>

---
+++ sql/Updates/z0472_r2300_scriptdevzero.sql
@@ -0,0 +1,3 @@
+DELETE FROM script_texts WHERE entry=-1000716;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1000716,'To Nighthaven! Keep your army close, champion. ',0,0,0,0,'remulos SAY_REMULOS_INTRO_2'),
