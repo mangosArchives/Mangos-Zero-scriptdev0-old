cd2f4366fe3c22249705c7753a444723a4146fe5
Configure Your Name <name@domain.com>
2012-07-08 12:30:58 +0200

    - Fix compilation error.
    Signed-off-by: TheLuda <theluda@getmangos.com>

--- include/sc_creature.cpp
+++ include/sc_creature.cpp
@@ -403,7 +403,7 @@ void ScriptedAI::DoTeleportPlayer(Unit* pUnit, float fX, float fY, float fZ, flo
 
     if (pUnit->GetTypeId() != TYPEID_PLAYER)
     {
-        error_log("SD0: Creature " UI64FMTD " (Entry: %u) Tried to teleport non-player unit (Type: %u GUID: " UI64FMTD ") to x: %f y:%f z: %f o: %f. Aborted.", m_creature->GetObjectGuid(), m_creature->GetEntry(), pUnit->GetTypeId(), pUnit->GetObjectGuid(), fX, fY, fZ, fO);
+        error_log("SD0: Creature %s tried to teleport non-player (%s) to x: %f y:%f z: %f o: %f. Aborted.", m_creature->GetGuidStr().c_str(), pUnit->GetGuidStr().c_str(), fX, fY, fZ, fO);
         return;
     }
 
