f5b95f67e2f661859e08468de63567aee0fbf97b
TheLuda <theluda@getmangos.com>
2012-09-25 17:10:52 +0200

    - Unified file headers.
    Signed-off-by: TheLuda <theluda@getmangos.com>

--- include/sc_grid_searchers.cpp
+++ include/sc_grid_searchers.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
- * Copyright (C) 2010-2011 ScriptDev0 <http://github.com/mangos-zero/scriptdev0>
+ * Copyright (C) 2006-2012 ScriptDev2 <http://www.scriptdev2.com/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
